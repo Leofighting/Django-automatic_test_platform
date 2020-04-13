f27a0e26c125441fdcd40a4723e1a16d707a4977
leoxiaojw <leoxiaojw@gmail.com>
2020-04-13 22:15:51 +0800

    接口测试

--- apitest/tests.py
+++ apitest/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.http import HttpResponse
 
-# Create your tests here.
+from apitest.views import test, home, login
+
+
+class titlePageTest(TestCase):
+    def test_loginpage_return_title_html(self):
+        request = HttpResponse()
+        response = login(request)
+        self.assertIn(b"<title>AutotestPlat</title>", response.content)
+
